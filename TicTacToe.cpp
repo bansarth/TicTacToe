abb9a0a26666d4e328d695890a06f5674b9ac9b9
bansarth <sarthakb42@gmail.com>
2019-08-03 11:52:02 -0500

    Completed game mode selection

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 int main() {
+	cout << "Hello, welcome to TicTacToe\n"
+		"Which version of TicTacToe would you like to play:\n"
+		"1. Two Player Mode\n"
+		"2. Player vs. Bot (Easy Mode)\n"
+		"3. Player vs. Bot (Hard Mode)\n";
+	int version;
+	cin >> version;
 	TTT board;
-	board.levelTwoBot();
+	if (version == 1) {
+		board.twoPlayerMode();
+	}
+	else if (version == 2) {
+		board.levelOneBot();
+	}
+	else if (version == 3) {
+		board.levelTwoBot();
+	}
 }
